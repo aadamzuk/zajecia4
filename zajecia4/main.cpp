fff309a8587c948e67c64f3e24e9a6e538318971
aadamzuk <32269564+aadamzuk@users.noreply.github.com>
2017-10-16 16:36:06 +0200

    RGTD

---
+++ zajecia4/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+struct probka
+{
+    double t;
+    double x;
+    probka (double _t, double _x){
+    t=_t;
+    x=_x;
+    }
+
+};
+vector <probka> wczytaj (string nazwa)
+{
+    vector <probka> tab;
+    ifstream plik(nazwa);
+    {
+
+        string linia;
+        while(getline(plik,linia)){
+            cout<<linia<<endl;
+        }
+    }
+    plik.close();
+    return tab;
+}
+void zapis (vector <probka> dane,string nazwa)
+{
+    ofstream zapisanie(nazwa);
+    for(int i=0;i<dane.size();i++)
+    {
+
+    zapisanie<<dane[i].t<<endl;
+    }
+zapisanie.close();
+
+}
+//vector <probka> tablica;
+//tablica push_back(p1);
+//cout<<tablica[0];
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) { return -1;
+}
+string nazwa_pliku = argv[1];
+vector<probka>dane=wczytaj(argv[1]);
+zapis(dane,"nazwa.csv");
+cin.get();
+
+return 0;
+}
